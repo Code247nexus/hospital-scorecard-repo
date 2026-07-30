3b14b0dc373c6178b12f6ae78edac9f73c93e315
ADITYA JHA <mnipt49265@gmail.com>
2026-07-30 13:31:09 +0530

    Updated  settings

--- Config/settings.py
+++ Config/settings.py
@@ -16,10 +16,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv(
-    "ALLOWED_HOSTS",
-    "localhost,127.0.0.1"
-).split(",")
+ALLOWED_HOSTS = ["*"] if DEBUG else [
+    "hospital-scorecard-edwb.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://hospital-scorecard-edwb.onrender.com",
